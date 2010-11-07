65fd8f86b88bf496adfe27f3842cc0e302c3aef2
Paul Tepper <paul.tepper@gmail.com>
2010-11-06 20:23:14 -0400

    Lots of changes for wiki links. External stylesheet.

--- landlord-lookup/handler.php
+++ landlord-lookup/handler.php
@@ -1,16 +1,33 @@
 <?php 
+/************
+* @author Paul Tepper <paul.tepper@gmail.com>
+*
+************/
 
 include('simple_html_dom.php');
-$no =  $_GET['number'];
-$street = urlencode($_GET['street']);
-$boro =  $_GET['boro'];
 
-/*Manhattan
-Bronx
-Brooklyn
-Queens
-Staten Island
-*/
+if($_GET['fromwiki']=='true'){
+	$street = str_replace('_',' ',$_GET['address']);
+	$sp = explode(' ',$street);
+	$no = $sp[0];
+	$street = implode(' ',array_slice($sp,1));
+	
+	$boros = array(
+		'Manhattan' => 1,
+		'Bronx' => 2,
+		'Brooklyn' => 3,
+		'Queens' => 4,
+		'Staten Island' => 5
+	);
+	$boro = $boros[$_GET['boro']];
+}
+else{
+	$no =  $_GET['number'];
+	$street = $_GET['street'];
+	$boro =  $_GET['boro'];
+}
+
+$street = urlencode($street);
 
 $url1 = "http://a810-bisweb.nyc.gov/bisweb/PropertyProfileOverviewServlet?boro=$boro&houseno=$no&street=$street&go2=+GO+&requestid=0";
 
@@ -52,28 +69,36 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $str = curl_exec($ch);
 curl_close($ch);
 
-echo "<table id='table'>
+$content = "<table id='table'>
 <tr><td>Block:</td><td>$block</td></tr>
-<tr><td>Lot:</td><td>$lot</td></tr>
+<tr class='alt'><td>Lot:</td><td>$lot</td></tr>
 <tr><td>Owner:</td><td>$owner</td></tr>
 ";
 
 if($str!=''){
 	$html = str_get_html($str); 
-	$addr = $html->find('table[id=tblAddr]',0)->find('tr',1)->children(0);
+	$addr = $html->find('table[id=tblAddr]',0)->find('tr',1)->children(0)->innertext;
 
-	echo "<tr>
-		<td>NYS Department of State<br/> 
+	$content.= "<tr class='alt'>
+		<td class='label'>NYS Department of State<br/> 
 		Divsion of Corporations<br/> 
 		Search Results:</td>
 		<td>$addr</td>
 	</tr>
 	";
 }
-echo "
+$content.= "
 </table>
 ";
 
 ?>
-
-
+<html>
+	<head>
+		<title>Landlord Lookup Results</title>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+	<body>
+<h1>Landlord Lookup</h1>
+<?php echo $content; ?>
+	</body>	
+</html
